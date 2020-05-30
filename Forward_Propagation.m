3e2777730c843c741ea0cb586c27b8f4f4b46f6a
Munib Ahmed <53575827+Munib01@users.noreply.github.com>
2020-05-30 23:36:29 +0500

    Delete Forward_Propagation.m

--- Forward_Propagation.m
+++
@@ -1,8 +0,0 @@
-function [A_1, A_2] = Forward_Propagation(X_train, W_1, b_1, W_2, b_2)
-  
-Z_1 = Product(X_train, W_1, b_1);
-A_1 = sigmoid(Z_1);
-
-Z_2 = Product(A_1, W_2, b_2);
-A_2 = sigmoid(Z_2);
-end
