d499f853d51759e936ebffaf496c4ef5473ead65
Munib Ahmed <53575827+Munib01@users.noreply.github.com>
2020-07-20 23:03:02 +0500

    Update Forward_Propagation.m

--- digit_detect/Forward_Propagation.m
+++ digit_detect/Forward_Propagation.m
@@ -2,7 +2,6 @@ function [A_1, A_2] = Forward_Propagation(X_train, W_1, b_1, W_2, b_2)
   
 Z_1 = Product(X_train, W_1, b_1);
 A_1 = sigmoid(Z_1);
-
 Z_2 = Product(A_1, W_2, b_2);
 A_2 = sigmoid(Z_2);
 
