3e900eec499dfa940f947467868e32a9b4eaa032
Munib Ahmed <53575827+Munib01@users.noreply.github.com>
2020-07-27 23:37:50 +0500

    Update accuracy_with_tan_func.m

--- additional_files/accuracy_with_tan_func.m
+++ additional_files/accuracy_with_tan_func.m
@@ -1,4 +1,5 @@
 function performance = accuracy(W_2, W_1, b_2, b_1, X_train, Y_train_1)
+
 summy = 0;
 
 Z_1 = Product(X_train, W_1, b_1);
@@ -10,11 +11,11 @@ A_2 = sigmoid(Z_2);
 [a,b] = max(A_2,[],2);
 
 for i =1:1:size(X_train,1)
-  if(b(i) == Y_train_1(i))
-  
-  summy = summy+1;
 
+  if(b(i) == Y_train_1(i))
+  summy = summy+1;
 end
+
 end
 
 performance = (summy*100)/size(X_train,1);
